d32afe0b782033c978ba3294a2eb0fd266b35a3a
Dustin0005 <89694449+Dustin0005@users.noreply.github.com>
2021-08-28 23:41:02 +1000

    Add files via upload

---
+++ DNRPFW/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'FW by Dustin Newham (Dustin#0005) made for CDOJRP or Paid' -- Do not Remove
+
+client_scripts {
+  'client/civ.lua'
+}
+
+server_scripts {
+  'server/server.lua'
+}
+
+export "checkdutystatus"
